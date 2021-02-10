7fa55398850f6c83518a5806cb2b2577dd7fee9b
enescakr55 <enescakr01@hotmail.com>
2021-02-10 22:31:45 +0300

    10.02.2021 - 10.Ders -> ErrorResult Eklendi

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
 using Entities.DTOs;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,7 @@ namespace Business.Abstract
         List<Product> GetAllByCategoryId(int id);
         List<Product> GetByUnitPrice(decimal min, decimal max);
         List<ProductDetailDTO> GetProductDetails();
+        IResult Add(Product product);
+        Product GetById(int id);
     }
 }
--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
@@ -16,6 +17,17 @@ namespace Business.Concrete
         {
             _productDal = productDal;
         }
+
+        public IResult Add(Product product)
+        {
+            if (product.ProductName.Length < 2)
+            {
+                return new ErrorResult("Ürün ismi en az 2 karakter olmalıdır");
+            }
+            _productDal.Add(product);
+            return new Result(true, "Ürün eklendi");
+        }
+
         public List<Product> GetAll()
         {
             return _productDal.GetAll();
@@ -24,6 +36,11 @@ namespace Business.Concrete
         public List<Product> GetAllByCategoryId(int id)
         {
             return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.Get(p => p.ProductId == id);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
