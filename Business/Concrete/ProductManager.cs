3f42d33a8a96a72d0842046da422d275b211f6f1
enescakr55 <enescakr01@hotmail.com>
2021-02-14 00:01:50 +0300

    13 Şubat 3. Ders Sonu--> WebAPI eklendi.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -31,10 +31,10 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if(DateTime.Now.Hour == 22)
-            {
-                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
-            }
+           // if (DateTime.Now.Hour == 23)
+           // {
+            //    return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+           // }
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductsListed);
         }
 
